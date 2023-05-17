41a1a8fff847cbaee828ebe3758fa5fa6de33fc0
Julio <julio18@biyuyo.org>
2023-05-17 15:40:05 -0400

    General refact

--- src/Models/classroomStudent.model.ts
+++ src/Models/classroomStudent.model.ts
@@ -1,10 +1,8 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, ObjectLiteral, DeepPartial, ManyToOne, JoinColumn, Index, UpdateDateColumn } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, ManyToOne, JoinColumn, Index, UpdateDateColumn } from "typeorm";
 import { Classroom } from "./classroom.model";
 import { Student } from "./student.model";
 import { IsNotEmpty, IsInt } from "class-validator";
 import { Model } from "../Base/model";
-import { HTTP_STATUS } from "../Base/statusHttp";
-//import AppDataSource from "../database/database";
 
 @Entity()
 export class ClassroomStudent{
@@ -41,20 +39,5 @@ export class ClassroomStudent{
 }
 
 export class ClassroomStudentModel extends Model{
-
-    async post_validation(data:DeepPartial<ObjectLiteral>):Promise<ObjectLiteral> {
-        const classroom = await this.getById(Classroom,data.id_classroom);
-        const student = await this.getById(Student,data.id_student);
-
-        if(!classroom){
-            return {error: "classroom not found", status: HTTP_STATUS.BAD_RESQUEST};
-        }
-        if(!student){
-            return {error: "student not found", status: HTTP_STATUS.BAD_RESQUEST};
-        }
-
-        const classroomStudent = await this.create(ClassroomStudent,data);
-        return {classroomStudent, status:HTTP_STATUS.CREATED};
-    }
 
 }
