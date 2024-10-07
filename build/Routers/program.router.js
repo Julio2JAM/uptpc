f3bafbd1100e746f66fa3aafef59624a52f0b7e4
Julio Alvarez <julio18@biyuyo.org>
2024-10-07 15:56:31 -0400

    Remove dir build

--- build/Routers/program.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const program_controller_1 = require("../Controllers/program.controller");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-const controller = new program_controller_1.ProgramController();
-router.get('/', controller.get);
-router.post('/', controller.post);
-exports.default = router;
--- build/Routers/role.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const role_controller_1 = require("../Controllers/role.controller");
-const router = (0, express_1.Router)();
-const controller = new role_controller_1.RoleController();
-router.get('/', controller.get);
-router.post('/', controller.post);
-exports.default = router;
